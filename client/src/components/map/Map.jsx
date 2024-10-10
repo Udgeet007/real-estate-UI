2cfa09f9913c39c516b1f0cd3827e816395caf2b
Udgeet <bhatt8udgeet@gmail.com>
2024-10-11 01:03:27 +0530

    ListPage and Map Done.

---
+++ client/src/components/map/Map.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/prop-types */
+import "./map.scss";
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import Pin from "../pin/Pin";
+
+export default function Map({ items }) {
+  return (
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={7}
+      scrollWheelZoom={false}
+      className="map"
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {/* <Marker position={[51.505, -0.09]}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker> */}
+      {items.map((item) => (
+        <Pin item={item} key={item.id} />
+      ))}
+    </MapContainer>
+  );
+}
