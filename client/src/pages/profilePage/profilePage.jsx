13d1358a6960e8783e0680c9867a7d0336cbfa8a
Udgeet <bhatt8udgeet@gmail.com>
2024-10-12 16:27:59 +0530

    Done Chat Window Design.

--- client/src/pages/profilePage/profilePage.jsx
+++ client/src/pages/profilePage/profilePage.jsx
@@ -1,3 +1,4 @@
+import Chat from "../../components/chat/Chat";
 import List from "../../components/list/List";
 import "./profilePage.scss";
 
@@ -33,7 +34,9 @@ export default function ProfilePage() {
         </div>
       </div>
       <div className="chatContainer">
-        <div className="wrapper"></div>
+        <div className="wrapper">
+          <Chat/>
+        </div>
       </div>
     </div>
   );
